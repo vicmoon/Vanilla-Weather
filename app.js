a2fbe6208ac775aa7503f9e08cb9a512d715f67f
Victoria _Moonteanu <munteanuvictoria1@gmail.com>
2022-08-22 17:11:45 +0200

    updated css and js

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+function formatDate(timestamp){
+    let date =new Date(timestamp);
+    let hours = date.getHours();
+    if (hours <10){
+        hours =`0${hours}`;
+    }
+    let minutes =date.getMinutes();
+    if (minutes <10){
+        minutes=`0${minutes}`;
+    } 
+    
+    let days= ["Sunday", "Monday","Tuesday","Wednesday" ,"Thursday", "Friday","Saturday"];
+    let day =days[date.getDay()];
+
+    return `${day} ${hours} : ${minutes}`
+}
+
+
 function displayTemperature(response){
 console.log(displayTemperature);
 console.log(response.data); 
@@ -6,6 +24,7 @@ let cityElement=document.querySelector("#your-city");
 let descriptionElement=document.querySelector("#description");
 let humidityElement=document.querySelector("#humidity");
 let windElement=document.querySelector("#wind");
+let dateElement=document.querySelector("#date");
 
 
 descriptionElement.innerHTML=response.data.weather[0].description;
@@ -13,6 +32,7 @@ temperatureElement.innerHTML=Math.round(response.data.main.temp);
 cityElement.innerHTML=response.data.name;
 humidityElement.innerHTML=response.data.main.humidity;
 windElement.innerHTML=Math.round(response.data.wind.speed);
+dateElement.innerHTML=formatDate(response.data.dt*1000);
 }
 
 let apiKey ="a797ee45374c07f8f53469ef56ba3a8c"; 
