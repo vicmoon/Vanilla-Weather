cb63f53b820e8b5439dcb7342ea467e814590e0c
Victoria _Moonteanu <munteanuvictoria1@gmail.com>
2022-08-22 17:38:50 +0200

    updated icon

--- app.js
+++ app.js
@@ -17,15 +17,14 @@ function formatDate(timestamp){
 
 
 function displayTemperature(response){
-console.log(displayTemperature);
-console.log(response.data); 
+    console.log(response.data);
 let temperatureElement=document.querySelector("#temperature");
 let cityElement=document.querySelector("#your-city");
 let descriptionElement=document.querySelector("#description");
 let humidityElement=document.querySelector("#humidity");
 let windElement=document.querySelector("#wind");
 let dateElement=document.querySelector("#date");
-
+let iconElement=document.querySelector("#icon");
 
 descriptionElement.innerHTML=response.data.weather[0].description;
 temperatureElement.innerHTML=Math.round(response.data.main.temp);
@@ -33,11 +32,15 @@ cityElement.innerHTML=response.data.name;
 humidityElement.innerHTML=response.data.main.humidity;
 windElement.innerHTML=Math.round(response.data.wind.speed);
 dateElement.innerHTML=formatDate(response.data.dt*1000);
+iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
 }
 
 let apiKey ="a797ee45374c07f8f53469ef56ba3a8c"; 
-let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=Prague&appid=${apiKey}&units=metric`;
+let city ="Paris";
+let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature); 
